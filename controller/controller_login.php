434f55e1fe0bac6f0a89e6d6be53b8677ea6d7ee
Hiroshi-Gloria <sephirothMH@outlook.com>
2022-05-13 00:15:31 -0500

    chore: formated code

--- controller/controller_login.php
+++ controller/controller_login.php
@@ -1,21 +1,21 @@
-  <?php
- session_start();
- include("../models/class_db.php");
+<?php
+  session_start();
+  include("../models/class_db.php");
+  include("../models/class_usuarios_dal.php");
+  include("../models/class_usuarios.php");
 
-if (!empty($_POST['name']) && !empty($_POST['password'])) {
-  $records = $conn->prepare('SELECT id, nombre, contraseña FROM usuarios WHERE nombre = :name');
-  $records->bindParam(':name', $_POST['name']);
-  $records->execute();
-  $results = $records->fetch(PDO::FETCH_ASSOC);
-
-  $message = '';
-
-  if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-    $_SESSION['user_id'] = $results['id'];
-    header("Location: ./../view/index.php");
-  } else {
-    $message = 'Las credenciales no coinciden';
+  if (!empty($_POST['name']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT id, nombre, contraseña FROM usuarios WHERE nombre = :name');
+    $records->bindParam(':name', $_POST['name']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+    $message = '';
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      $_SESSION['user_id'] = $results['id'];
+      header("Location: /ProyectoWeb/views/index.php");
+    }else{
+      $message = 'Las credenciales no coinciden';
+      echo $message;
+    }
   }
-}
-
 ?>
--- models/class_db.php
+++ models/class_db.php
@@ -2,7 +2,7 @@
 $server = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'webhosting';
+$database = 'wbhostng';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
