b19a1b5e7070c32c1e3e70b988c67f61c9dc8fee
LucelyFlores <lucelyflores28@gmail.com>
2022-05-14 00:35:15 -0500

    Modificación controllers

---
+++ controller/controller_inserta_actualiza_usuarios.php
@@ -0,0 +1,85 @@
+<?php
+if (!empty($_POST)){
+	require_once 'php/funciones_php.php';
+    include("models/class_usuarios_dal.php");
+	$metodos_usuario=new usuarios_dal;
+
+	if (isset($_POST['f_id'])){
+		$usuario_id=strtoupper($_POST['f_id']);
+	}else{
+		$usuario_id=null;
+		echo "$usuario_id";
+		echo "no llego datos del Id del usuario";
+		exit;
+	}
+
+	if (isset($_POST['f_nombre'])){
+		$nomb=strtoupper($_POST['f_nombre']);
+	}else{
+		$nomb=null;
+		echo "no llego dato de nombre";
+		exit;
+	}
+
+	if (isset($_POST['f_contraseña'])){
+		$contra=strtoupper($_POST['f_contraseña']);
+	}else{
+		$contra=null;
+		echo "no llego dato de contraseña";
+		exit;
+	}
+
+	
+	$errores=array();
+	if ($_SERVER['REQUEST_METHOD']=='POST'){
+
+		if(!validaRequerido($nomb)){
+            $errores[]="El campo Nombre llegó vacío";
+        }
+        if(!validaRequerido($contra)){
+            $errores[]="El campo Contraseña llegó vacío";
+        }
+
+		if (!$errores){
+			$obj_usuario=new ticket_turno($usuario_id,$nomb);
+			if ($usuario_id==''){
+
+				if($metodos_usuario->insertar_usuario($obj_usuario)=="1"){
+					echo 'ok';
+				}
+				else{
+					print "Ocurrio un error para ingresar el usuario, intente nuevamente";
+				}
+
+			}else{
+				if($metodos_usuario->actualiza_usuario($obj_usuario)=="1"){
+					echo 'ok';
+				}
+				else{
+					print "Ocurrio un error para actualizar el usuario, intente nuevamente";
+				}
+			}
+
+
+		}else{
+			echo '<ul style="color: #f00;font-size:25px;">';
+      		foreach ($errores as $error):
+         	echo '<li>'.$error.'</li>';
+      		endforeach;
+   			echo '</ul>';
+		}
+
+
+
+	}
+	else{
+		print "no ocurrio el request method";
+	}
+
+
+}
+else{
+	echo 'Error de post, los datos no llegaron correctamente ';
+}
+
+?>
