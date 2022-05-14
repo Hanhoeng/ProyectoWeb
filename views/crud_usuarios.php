a7ae12fb8d4f35d4cd3e321edfb5785e1fce03f7
LucelyFlores <lucelyflores28@gmail.com>
2022-05-14 15:45:49 -0500

    Inclusiones en crud usuarios

--- views/crud_usuarios.php
+++ views/crud_usuarios.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <?php include_once "../inclusiones/meta_tags.php"; ?>
 	<link rel="stylesheet" href="../css/estilos2.css">	
-
 	<?php include_once "../inclusiones/css.php"; ?>
-
     <title>The Web Hosting</title>
 </head>
 <body background="../css/imagenes/fondo.png">
 <section id="contenedor">
 <a id="ver" href="#">
-
-<picture>
-<header>
-<a href="index.php"><img id="logo" src="./../css/imagenes/logo (1).png"></a>
-<a href="compañia.php"><img id="compañia" src="../css/imagenes/compañia.png"></a>
-<a href="servicios.php"><img id="servicios" src="../css/imagenes/servicios.png"></a>
-<a href="soluciones.php"><img id="soluciones" src="../css/imagenes/soluciones.png"></a>
-<a href="clientes.php"><img id="clientes" src="../css/imagenes/clientes.png"> </a>
-<a href="contacto.php"><img id="contacto" src="../css/imagenes/contacto.png"></a> 	
-</header>
- </picture>
-
-<br>
-<br>
-<br>
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        include_once "../inclusiones/menu_horizontal_superior.php";
+        ?>
+    </div>
+</div>
 
 <!--<div class="container-fluid">  -->
 <div class="container" style="margin-top: 65px !important;">  
@@ -101,6 +90,8 @@ $result_dato_catalogo_usuarios=$obj_dato_catalogo_usuarios->obtener_lista_usuari
 </table>
 </div>
 
+
+<?php include_once "../inclusiones/js_incluidos.php"; ?>
 
 </div><!-- end container -->  
 
