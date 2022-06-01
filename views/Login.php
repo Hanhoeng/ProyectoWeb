ae0efa282bd0fd8c9e9e446bbcaec93f6d1d2ce6
FernandoPerez-ops <feroloci@gmail.com>
2022-05-31 19:24:11 -0500

    reCAPTCHA

--- views/Login.php
+++ views/Login.php
@@ -10,7 +10,7 @@
     <title>Login</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
-    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script async src="https://www.google.com/recaptcha/api.js"></script>
     <script>
       function onSubmit(token) {
         document.getElementById("Login").submit();
@@ -29,7 +29,8 @@
     <h1>Acceder</h1>
     <span>o <a href="singup.php">Registrarse</a></span>
     <form id="Login" method="POST" action="../controller/controller_login.php" onsubmit="return valida_login()" ;>
-      <!-- Usuario -->
+      
+    <!-- Usuario -->
       <div class="form-group">
           <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" required="True">
       </div>
@@ -38,23 +39,38 @@
           <input type="password" class="form-control" id="password" name="password" placeholder="Enter your Password" required="True">
       </div>
       <!-- Captcha -->
-      <button class="g-recaptcha"
-        id="captcha"
-        name="captcha"
-        data-sitekey="6LeGJw8gAAAAAMK11G3_jvFYoPV8PNhGdLG99Yfl" 
-        data-callback='onSubmit' 
-        data-action='submit'>Acceder</button>
-      
-      <script>
-        function onClick(e) {
-          e.preventDefault();
-          grecaptcha.ready(function() {
-            grecaptcha.execute('6LeGJw8gAAAAAMK11G3_jvFYoPV8PNhGdLG99Yfl', {action: 'submit'}).then(function(token) {
-                
-            });
-          });
+      <style>
+        .text-center {
+          text-align: center;
         }
-      </script>
+
+        .g-recaptcha {
+          display: inline-block;
+        }
+      </style>
+
+      
+<form method="post" onsubmit="return submitUserForm();">
+    <div class="g-recaptcha" data-sitekey="6LdJbTQgAAAAAHWdWny1Irk009npUdDg5DzBTsCp" data-callback="verifyCaptcha"></div>
+    <div id="g-recaptcha-error"></div>
+    <input type="submit" name="submit" value="Submit" />
+</form>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<script>
+var recaptcha_response = '';
+function submitUserForm() {
+    if(recaptcha_response.length == 0) {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
+    return true;
+}
+ 
+function verifyCaptcha(token) {
+    recaptcha_response = token;
+    document.getElementById('g-recaptcha-error').innerHTML = '';
+}
+</script>
 
     </form>
     <?php
